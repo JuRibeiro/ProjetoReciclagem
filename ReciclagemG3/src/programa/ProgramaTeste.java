8db898034f13644e50799b6e326daee5c6ae6d39
fconstancio <constanciosalomao@gmail.com>
2021-05-13 17:17:48 -0300

    corrigindo engano

---
+++ ReciclagemG3/src/programa/ProgramaTeste.java
@@ -0,0 +1,12 @@
+package programa;
+
+import classes.Papel;
+
+public class ProgramaTeste {
+	public static void main(String[] args) {
+		
+		Papel papel = new Papel(10, 0.14);
+		System.out.print(papel.Reciclagem());
+		
+	}
+}
