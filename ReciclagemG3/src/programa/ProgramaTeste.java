b900c1e74d2a6cb6d5b1c89f604228601e67cec0
Renato-Nakamura <renato.yukio30@hotmail.com>
2021-05-13 21:00:14 -0300

    interface 0.2

--- ReciclagemG3/src/programa/ProgramaTeste.java
+++ ReciclagemG3/src/programa/ProgramaTeste.java
@@ -12,6 +12,7 @@ public class ProgramaTeste {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner leia = new Scanner(System.in);
+		boolean continuar=true;
 		
 		//Papel papel = new Papel(10, 0.14);
 		//System.out.print(papel.Reciclagem());
@@ -21,11 +22,22 @@ public class ProgramaTeste {
 		
 		System.out.println("PONTOS PELA TERRA");
 		System.out.println("Pressione Enter para iniciar :");
-		String x =leia.nextLine();
+		String x = leia.nextLine();
 		
 		
 		System.out.println("Bem vindo ao centro de coleta do PontosPelaTerra");
 		
+		do {
+		System.out.print("Escolha o material que deseja reciclar : \n1-Metal\n2-Papel\n3-Plastico\n4-Vidro\n");
+		//if para escolha do material
+		
+		
+		
+		System.out.println("Deseja reciclar mais materiais? 1-Sim | 2-Não");
+		continuar = (leia.next().charAt(0)=='1'); // ele pega o valor inserido e analisa se é igual a 1, caso seja continuar recebe true.
+		}while(continuar);
+		
+		System.out.println("Obrigado pela contribuição!!");
 		
 	}
 }
